cd186e9a1cf8f2d3939fedda14ea735649f031b4
Guillaume Aubertin <guillaume.aubertin@gmail.com>
2016-08-26 12:00:37 +0200

    adding configuration file and some cleanup
    Signed-off-by: Guillaume Aubertin <guillaume.aubertin@gmail.com>

--- temperature_daemon.py
+++ temperature_daemon.py
@@ -2,16 +2,17 @@
 import sys, os, time, signal
 import sqlite3, datetime
 
+SOCKET_CODE_ON = 0
+SOCKET_CODE_OFF = 0
+
 def socket_command(value):
 
-    if (value == 1):
-        cmd = '/home/pi/work/rfoutlet.git/codesend 1054003'
-    else:
-        cmd = '/home/pi/work/rfoutlet.git/codesend 1054012'
+    cmd = "/home/pi/work/rfoutlet.git/codesend %d" % value
     os.system(cmd)
-    notification = "/bin/bash /home/pi/python/notify.sh"
-    os.system(notification)
-    return value
+    if value == SOCKET_CODE_ON:
+        return 1
+    else:
+        return 0
 
 def dump_db():
 
@@ -21,33 +22,42 @@ def dump_db():
     for data in c.fetchall():
         print data
 
-def sig_handler(signal, frame):
-
-    print "cleaning up before exiting..."
-    dump_db()
-    conn.close()
-    sys.exit(0)
-
 if __name__ == '__main__':
 
-    #TODO: daemonize (double fork, start/stop/restart)
-    signal.signal(signal.SIGINT, sig_handler)
-    socket_status = socket_command(0)
-    val = 0
+    # get configuration
+    if (len(sys.argv) != 2):
+        print "configuration file needed"
+        sys.exit(0)
+
+    fd_config = open(sys.argv[1], 'r')
+    for line in fd_config.readlines():
+        line = " ".join(line.split())
+        line = line.split(" ")
+        if (line[0] == "W1_PATH"):
+            print "W1_PATH :", line[1]
+            W1_PATH = line[1]
+        if (line[0] == "SOCKET_CODE_ON"):
+            print "SOCKET_CODE_ON :", line[1]
+            SOCKET_CODE_ON = int(line[1])
+        if (line[0] == "SOCKET_CODE_OFF"):
+            print "SOCKET_CODE_OFF :", line[1]
+            SOCKET_CODE_OFF = int(line[1])
 
     # database init
-    #TODO: add power switch status to the database
     conn = sqlite3.connect('beer_machine.db')
     c = conn.cursor()
     c.execute('''CREATE TABLE IF NOT EXISTS fermentation_temp
-             (date text, temperature float)''')
+             (date text, temperature float, switch int)''')
     conn.close()
+
+    # before getting started, we switch off the plug to be in a known state
+    socket_status = socket_command(SOCKET_CODE_OFF);
 
     # infinite loop to do stuff
     while True:
 
         # read file from 1-wire sysfs and extract temperature
-        fd = open("/sys/bus/w1/devices/28-031600cde1ff/w1_slave", "r")
+        fd = open(W1_PATH, "r")
         temp_str = fd.read()
         fd.close()
         temp_str = temp_str.split()
@@ -58,18 +68,19 @@ if __name__ == '__main__':
         # store in db
         conn = sqlite3.connect('beer_machine.db')
         c = conn.cursor()
-        c.execute("INSERT INTO fermentation_temp VALUES (?, ?)", (datetime.datetime.now(), temp))
+        c.execute("INSERT INTO fermentation_temp VALUES (?, ?, ?)", (datetime.datetime.now(), temp,
+                                                                     socket_status))
         conn.commit()
         conn.close()
 
         # adjust temperature with socket
         # TODO: use pid
         if temp > 20.5:
-            val = 1
+            command = SOCKET_CODE_ON
         if temp < 20:
-            val = 0
-        if (val != socket_status):
+            command = SOCKET_CODE_OFF
+        if (command != socket_status):
             print "switching power socket !"
-            socket_status = socket_command(val);
+            socket_status = socket_command(command);
 
         time.sleep(30)
