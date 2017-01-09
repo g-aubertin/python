0f92997667aed76bfaaa340963ba42b524980456
Guillaume Aubertin <guillaume.aubertin@gmail.com>
2017-01-09 11:38:39 +0100

    cleaning beer machine files
    Signed-off-by: Guillaume Aubertin <guillaume.aubertin@gmail.com>

--- notify.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-curl -X POST  \
-     -H "X-Parse-Application-Id: HQrMLZDevpTv2J1raSC6KATvlpNqqePPecUE0EgG" \
-     -H "X-Parse-REST-API-Key: ivgV8ZoA0kyOOLWKms3M0wxYUxyUw4tfGgbj6DFd" \
-     -H "Content-Type: application/json" \
-     -d "{\"key\":\"$KEY\", \"text\": \"socket switch \"}"  \
-     https://api.parse.com/1/functions/notify
